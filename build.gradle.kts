7b777bb711f690c3868f5bc0a0ddebfb68912142
Cosmic-Coding-Community-Club <cosmiccodingcommunityclub@gmail.com>
2022-11-15 16:49:21 +0100

    Base Spring boot Project

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	
-	
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
