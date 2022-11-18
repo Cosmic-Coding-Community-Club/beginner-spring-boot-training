3804aca1d5edacda725b38b5260122ccca3d74ae
Cosmic-Coding-Community-Club <cosmiccodingcommunityclub@gmail.com>
2022-11-18 10:15:14 +0100

    create tests for this practice

--- src/test/kotlin/com/cccc/beginnerspringboottraining/ApplicationTests.kt
+++ src/test/kotlin/com/cccc/beginnerspringboottraining/ApplicationTests.kt
@@ -1,13 +1,43 @@
 package com.cccc.beginnerspringboottraining
 
+import com.cccc.beginnerspringboottraining.practice2.Training
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    
+    @Autowired
+    @Qualifier("springBootTrainingWay1")
+    private lateinit var trainingWay1: Training
+    
+    @Autowired
+    @Qualifier("springBootTrainingWay2")
+    private lateinit var trainingWay2: Training
+    
+    @Autowired
+    @Qualifier("springBootTrainingWay3")
+    private lateinit var trainingWay3: Training
+    
+    @Test
+    fun shouldReturnTrainingsWhenApplyDifferentWaysToInstance() {
+        assertThat(trainingWay1)
+            .isNotNull
+            .usingRecursiveComparison()
+            .isEqualTo(trainingWay2)
+        
+        assertThat(trainingWay2)
+            .isNotNull
+            .usingRecursiveComparison()
+            .isEqualTo(trainingWay3)
+        
+        assertThat(trainingWay3)
+            .isNotNull
+            .usingRecursiveComparison()
+            .isEqualTo(trainingWay1)
+    }
+    
 }
