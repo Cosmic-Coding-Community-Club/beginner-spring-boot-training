d3c3fba50936e4e9ab80e9834ab077cc6d2f4fae
Cosmic-Coding-Community-Club <cosmiccodingcommunityclub@gmail.com>
2022-11-18 10:16:38 +0100

    improve code

--- src/main/kotlin/com/cccc/beginnerspringboottraining/Application.kt
+++ src/main/kotlin/com/cccc/beginnerspringboottraining/Application.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackages = ["com.cccc.beginnerspringboottraining.configuration"])
 class Application
 
 fun main(args: Array<String>) {
--- src/test/kotlin/com/cccc/beginnerspringboottraining/ApplicationTests.kt
+++ src/test/kotlin/com/cccc/beginnerspringboottraining/ApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest
 class ApplicationTests {
 
 	@Test
-	fun contextLoads() {
+	fun shouldReturnTrainingsWhenApplyDifferentWaysToInstance() {
 	}
 
 }
