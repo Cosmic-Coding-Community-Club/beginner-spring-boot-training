7867d09efa11335b0060293e6663def27980500e
Cosmic-Coding-Community-Club <cosmiccodingcommunityclub@gmail.com>
2022-11-19 08:31:43 +0100

    remove solutions

--- src/main/kotlin/com/cccc/beginnerspringboottraining/practice3/infrastructure/pokemon/controller/PokemonDto.kt
+++
@@ -1,8 +0,0 @@
-package com.cccc.beginnerspringboottraining.practice3.infrastructure.pokemon.controller
-
-data class PokemonDto(
-    val name: String,
-    val attack: Int,
-    val defense: Int,
-    val speed: Int
-)
--- src/main/kotlin/com/cccc/beginnerspringboottraining/practice3/infrastructure/pokemon/controller/PokemonNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.cccc.beginnerspringboottraining.practice3.infrastructure.pokemon.controller
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-class PokemonNotFoundException(name: String) : RuntimeException("Pokemon Not found [$name]")
